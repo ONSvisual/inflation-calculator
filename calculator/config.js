77d8500075cf448b28887962cefcb0299db482e0
henryjameslau <henry.james.lau@gmail.com>
2022-03-11 10:13:22 +0000

    Initial commit

---
+++ calculator/config.js
@@ -0,0 +1,46 @@
+dvc={
+  "time_period" : "months",
+  "time_format": "%B %Y",
+  "cpih_time_format": "%Y %b",
+  "time_series_totalmnths": 36,
+  "essential": {
+    "colour_palette": [ "#27A0CC", "#003C57"],
+    "legendLabels": ["Spending less", "Spending more"]
+  },
+  "optional": {
+    "margin": {
+      "sm": {
+        "top": 20,
+        "right": 0,
+        "bottom": 35,
+        "left": 55
+      },
+      "md": {
+        "top": 20,
+        "right": 0,
+        "bottom": 20,
+        "left": 55
+      },
+      "lg": {
+        "top": 20,
+        "right": 90,
+        "bottom": 20,
+        "left": 55
+      }
+    },
+    "aspectRatio": {
+      "sm": [16, 16],
+      "md": [16, 7],
+      "lg": [16, 10]
+    },
+    "mobileBreakpoint": 414,
+    "mediumBreakpoint":600,
+
+    "x_num_ticks": {
+      "sm": 6,
+      "md": 8,
+      "lg": 10
+    }
+
+  }
+};
---
+++ calculator/inflation_data.js
@@ -0,0 +1,592 @@
+inflation_data = [
+ {
+   "cat_id": "foodhotdrinks",
+   "page": "Groceries",
+   "category": "Food, and hot and soft drinks",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L55P",
+   "weight_cdid": "L5CZ",
+   "average": 89
+ },
+ {
+   "cat_id": "spiritswinebeer",
+   "page": "Groceries",
+   "category": "Spirits, wines and beers",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56I",
+   "weight_cdid": "L5CZ",
+   "average": 18
+ },
+ {
+   "cat_id": "tobacco",
+   "page": "Groceries",
+   "category": "Tobacco products including electronic cigarettes",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56M",
+   "weight_cdid": "L5CZ",
+   "average": 17
+ },
+ {
+   "cat_id": "personalcare",
+   "page": "Groceries",
+   "category": "Personal care including personal hygiene products, toilet rolls, toothpaste, deodorants, etc.",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58Y",
+   "weight_cdid": "L5CZ",
+   "average": 19
+ },
+ {
+   "cat_id": "nongrocery",
+   "page": "Groceries",
+   "category": "Non-grocery items (e.g. washing-up liquid, household cleaner, bleach, foil, kitchen roll, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57G",
+   "weight_cdid": "L5CZ",
+   "average": 3
+ },
+ {
+   "cat_id": "ooh",
+   "page": "Housing",
+   "category": "Housing costs - OOH",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L5P7",
+   "weight_cdid": "L5PA",
+   "average": 187
+ },
+ {
+   "cat_id": "ooh",
+   "page": "Housing",
+   "category": "Housing costs - OOH",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56T",
+   "weight_cdid": "L5E6",
+   "average": 187
+ },
+ {
+   "cat_id": "rent",
+   "page": "Housing",
+   "category": "Rent",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56S",
+   "weight_cdid": "L5CZ",
+   "average": 74
+ },
+ {
+   "cat_id": "counciltax",
+   "page": "Housing",
+   "category": "Council Tax or rates",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "DHYU",
+   "weight_cdid": "L5CZ",
+   "average": 32
+ },
+ {
+   "cat_id": "electricitygasfuels",
+   "page": "Housing",
+   "category": "Electricity, gas and other fuels",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56Z",
+   "weight_cdid": "L5CZ",
+   "average": 26
+ },
+ {
+   "cat_id": "water",
+   "page": "Housing",
+   "category": "Water and sewerage",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L56W",
+   "weight_cdid": "L5CZ",
+   "average": 9
+ },
+ {
+   "cat_id": "mobilephone",
+   "page": "Communication",
+   "category": "Mobile phone charges (contract and purchase of phone, apps and accessories)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DK",
+   "weight_cdid": "J4LW",
+   "average": 7.56
+ },
+ {
+   "cat_id": "mobilephone",
+   "page": "Communication",
+   "category": "Mobile phone charges (contract and purchase of phone, apps and accessories)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DM",
+   "weight_cdid": "J4LZ",
+   "average": 7.56
+ },
+ {
+   "cat_id": "internet",
+   "page": "Communication",
+   "category": "Other communications costs (internet, landline telephone, packages)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L585",
+   "weight_cdid": "L5FG",
+   "average": 11.44
+ },
+ {
+   "cat_id": "internet",
+   "page": "Communication",
+   "category": "Other communications costs (internet, landline telephone, packages)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DJ",
+   "weight_cdid": "J4LV",
+   "average": 11.44
+ },
+ {
+   "cat_id": "internet",
+   "page": "Communication",
+   "category": "Other communications costs (internet, landline telephone, packages)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DL",
+   "weight_cdid": "J4LY",
+   "average": 11.44
+ },
+ {
+   "cat_id": "internet",
+   "page": "Communication",
+   "category": "Other communications costs (internet, landline telephone, packages)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DN",
+   "weight_cdid": "J4M2",
+   "average": 11.44
+ },
+ {
+   "cat_id": "internet",
+   "page": "Communication",
+   "category": "Other communications costs (internet, landline telephone, packages)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3DO",
+   "weight_cdid": "J4M3",
+   "average": 11.44
+ },
+ {
+   "cat_id": "vehicles",
+   "page": "Transport",
+   "category": "Purchase of vehicles (car, motorcycles or bikes)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57P",
+   "weight_cdid": "L5CZ",
+   "average": 33
+ },
+ {
+   "cat_id": "petroldiesel",
+   "page": "Transport",
+   "category": "Petrol or diesel",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57V",
+   "weight_cdid": "L5CZ",
+   "average": 21
+ },
+ {
+   "cat_id": "othermotoring",
+   "page": "Transport",
+   "category": "Other motoring costs including repairs, parking costs, road tolls and car or van hire",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57U",
+   "weight_cdid": "L5F7",
+   "average": 37
+ },
+ {
+   "cat_id": "othermotoring",
+   "page": "Transport",
+   "category": "Other motoring costs including repairs, parking costs, road tolls and car or van hire",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57W",
+   "weight_cdid": "L5F9",
+   "average": 37
+ },
+ {
+   "cat_id": "othermotoring",
+   "page": "Transport",
+   "category": "Other motoring costs including repairs, parking costs, road tolls and car or van hire",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57X",
+   "weight_cdid": "L5FA",
+   "average": 37
+ },
+ {
+   "cat_id": "trainfares",
+   "page": "Transport",
+   "category": "Train fares",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57Z",
+   "weight_cdid": "L5CZ",
+   "average": 5
+ },
+ {
+   "cat_id": "busfares",
+   "page": "Transport",
+   "category": "Bus fares",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L582",
+   "weight_cdid": "L5CZ",
+   "average": 8
+ },
+ {
+   "cat_id": "electrical",
+   "page": "Leisure",
+   "category": "Electrical items including TVs, laptops, tablet, cameras, along with CDs, DVDs, blu rays",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L587",
+   "weight_cdid": "L5CZ",
+   "average": 23
+ },
+ {
+   "cat_id": "tv",
+   "page": "Leisure",
+   "category": "Television licence fees and subscriptions",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3FL",
+   "weight_cdid": "L5CZ",
+   "average": 4.4
+ },
+ {
+   "cat_id": "games",
+   "page": "Leisure",
+   "category": "Games, toys and hobbies (Including computer games and consoles)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58G",
+   "weight_cdid": "L5CZ",
+   "average": 21
+ },
+ {
+   "cat_id": "entertainment",
+   "page": "Leisure",
+   "category": "Entertainment and days out (attractions, football admissions, gym membership, cinemas, concerts, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58L",
+   "weight_cdid": "L5CZ",
+   "average": 12.6
+ },
+ {
+   "cat_id": "entertainment",
+   "page": "Leisure",
+   "category": "Entertainment and days out (attractions, football admissions, gym membership, cinemas, concerts, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3FJ",
+   "weight_cdid": "J4X9",
+   "average": 12.6
+ },
+ {
+   "cat_id": "entertainment",
+   "page": "Leisure",
+   "category": "Entertainment and days out (attractions, football admissions, gym membership, cinemas, concerts, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3FK",
+   "weight_cdid": "J4XA",
+   "average": 12.6
+ },
+ {
+   "cat_id": "entertainment",
+   "page": "Leisure",
+   "category": "Entertainment and days out (attractions, football admissions, gym membership, cinemas, concerts, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3FM",
+   "weight_cdid": "J4XC",
+   "average": 12.6
+ },
+ {
+   "cat_id": "entertainment",
+   "page": "Leisure",
+   "category": "Entertainment and days out (attractions, football admissions, gym membership, cinemas, concerts, etc.)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "J3FN",
+   "weight_cdid": "J4XD",
+   "average": 12.6
+ },
+ {
+   "cat_id": "pets",
+   "page": "Leisure",
+   "category": "Pets including food, vets fees and purchase of small pets",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58J",
+   "weight_cdid": "L5CZ",
+   "average": 10
+ },
+ {
+   "cat_id": "gardening",
+   "page": "Leisure",
+   "category": "Gardening, plants and flowers",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58I",
+   "weight_cdid": "L5CZ",
+   "average": 5
+ },
+ {
+   "cat_id": "eatingout",
+   "page": "Leisure",
+   "category": "Eating and drinking out (pubs, restaurants, takeaways or canteens)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58S",
+   "weight_cdid": "L5CZ",
+   "average": 57
+ },
+ {
+   "cat_id": "holidays",
+   "page": "Leisure",
+   "category": "Holidays (including air and sea fares and UK hotel accomodation)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L583",
+   "weight_cdid": "L5FE",
+   "average": 24
+ },
+ {
+   "cat_id": "holidays",
+   "page": "Leisure",
+   "category": "Holidays (including air and sea fares and UK hotel accomodation)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L584",
+   "weight_cdid": "L5FF",
+   "average": 24
+ },
+ {
+   "cat_id": "holidays",
+   "page": "Leisure",
+   "category": "Holidays (including air and sea fares and UK hotel accomodation)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58R",
+   "weight_cdid": "L5G4",
+   "average": 24
+ },
+ {
+   "cat_id": "holidays",
+   "page": "Leisure",
+   "category": "Holidays (including air and sea fares and UK hotel accomodation)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58V",
+   "weight_cdid": "L5G8",
+   "average": 24
+ },
+ {
+   "cat_id": "otherleisure",
+   "page": "Leisure",
+   "category": "Other (including books, newspapers and stationary, person effects, sports equipment and major durable items)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58D",
+   "weight_cdid": "L5FO",
+   "average": 38
+ },
+ {
+   "cat_id": "otherleisure",
+   "page": "Leisure",
+   "category": "Other (including books, newspapers and stationary, person effects, sports equipment and major durable items)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58H",
+   "weight_cdid": "L5FS",
+   "average": 38
+ },
+ {
+   "cat_id": "otherleisure",
+   "page": "Leisure",
+   "category": "Other (including books, newspapers and stationary, person effects, sports equipment and major durable items)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58N",
+   "weight_cdid": "L5FY",
+   "average": 38
+ },
+ {
+   "cat_id": "otherleisure",
+   "page": "Leisure",
+   "category": "Other (including books, newspapers and stationary, person effects, sports equipment and major durable items)",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58Z",
+   "weight_cdid": "L5GC",
+   "average": 38
+ },
+ {
+   "cat_id": "health",
+   "page": "Other",
+   "category": "Health and personal care",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L55U",
+   "weight_cdid": "L5D6",
+   "average": 24
+ },
+ {
+   "cat_id": "health",
+   "page": "Other",
+   "category": "Health and personal care",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L58X",
+   "weight_cdid": "L5GA",
+   "average": 24
+ },
+ {
+   "cat_id": "education",
+   "page": "Other",
+   "category": "University tuition fees, private school fees and part-time leisure classes",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L55Y",
+   "weight_cdid": "L5CZ",
+   "average": 30
+ },
+ {
+   "cat_id": "socialprotection",
+   "page": "Other",
+   "category": "Social protection including childcare cost or retirement homes for elderly patients",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L594",
+   "weight_cdid": "L5CZ",
+   "average": 19
+ },
+ {
+   "cat_id": "clothes",
+   "page": "Other",
+   "category": "Clothes and footwear",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L55R",
+   "weight_cdid": "L5CZ",
+   "average": 59
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L576",
+   "weight_cdid": "L5EH",
+   "average": 46
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L579",
+   "weight_cdid": "L5EK",
+   "average": 46
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57A",
+   "weight_cdid": "L5EL",
+   "average": 46
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57D",
+   "weight_cdid": "L5EO",
+   "average": 46
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57E",
+   "weight_cdid": "L5EP",
+   "average": 46
+ },
+ {
+   "cat_id": "furniture",
+   "page": "Other",
+   "category": "Furniture, furnishings, household equipment (including household appliances) and tools",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L57H",
+   "weight_cdid": "L5ES",
+   "average": 46
+ },
+ {
+   "cat_id": "insurance",
+   "page": "Other",
+   "category": "Insurance and financial services",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L595",
+   "weight_cdid": "L5GG",
+   "average": 30
+ },
+ {
+   "cat_id": "insurance",
+   "page": "Other",
+   "category": "Insurance and financial services",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L599",
+   "weight_cdid": "L5GK",
+   "average": 30
+ },
+ {
+   "cat_id": "insurance",
+   "page": "Other",
+   "category": "Insurance and financial services",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L59B",
+   "weight_cdid": "L5GM",
+   "average": 30
+ },
+ {
+   "cat_id": "other",
+   "page": "Other",
+   "category": "Other expenditure",
+   "description": "",
+   "inf_values": [],
+   "inflation_cdid": "L55O",
+   "weight_cdid": "L5CZ",
+   "average": 0
+ }
+]
