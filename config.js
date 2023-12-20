e554f5408e191e87b20086e9e1022c51a3d59ae5
ONSSamBennett <89449489+ONSSamBennett@users.noreply.github.com>
2023-12-20 11:58:50 +0000

    Fix y axis
    Increase margin width for y axis to compensate for double digits

---
+++ config.js
@@ -0,0 +1,116 @@
+dvc = {
+  "deciles":[
+    {
+      "decile": "Bottom 10%",
+      "value": 988
+    },
+    {
+      "decile": "2nd",
+      "value": 1452
+    },
+    {
+      "decile": "3rd",
+      "value": 1933
+    },
+    {
+      "decile": "4th",
+      "value": 2461
+    },
+    {
+      "decile": "5th",
+      "value": 3068
+    },
+    {
+      "decile": "6th",
+      "value": 3783
+    },
+    {
+      "decile": "7th",
+      "value": 4554
+    },
+    {
+      "decile": "8th",
+      "value": 5616
+    },
+    {
+      "decile": "9th",
+      "value": 7510
+    },
+  ],
+  "quickcategories": ["foodhotdrinks","rent","ooh","electricitygasfuels","petroldiesel","eatingout","childcare","holidays","clothes","trainfares","busfares"],
+  "time_period": "months",
+  "time_format": "%B %Y",
+  "cpih_time_format": "%Y %b",
+  "time_series_totalmnths": 60,
+  "prop_colour_palette": ["#206095", "#27A0CC", "#003C57", "#118C7B", "#A8BD3A"],
+  "prop_colour_palette_text": ["#206095", "#27A0CC", "#003C57", "#118C7B", "#8A9B2E"],
+  "lineColours":["#A8BD3A","#206095"],
+  "lineChartMargin": {
+    "sm": {
+      "top": 10,
+      "right": 20,
+      "bottom": 20,
+      "left": 40
+    },
+    "md": {
+      "top": 10,
+      "right": 20,
+      "bottom": 20,
+      "left": 40
+    },
+    "lg": {
+      "top": 10,
+      "right": 20,
+      "bottom": 20,
+      "left": 40
+    }
+  },
+  "propChartMargin": {
+    "sm": {
+      "top": 0,
+      "right": 20,
+      "bottom": 20,
+      "left": 10
+    },
+    "md": {
+      "top": 0,
+      "right": 20,
+      "bottom": 20,
+      "left": 10
+    },
+    "lg": {
+      "top": 10,
+      "right": 20,
+      "bottom": 20,
+      "left": 10
+    }
+  },
+  "barChartMargin": {
+    "sm": {
+      "top": 10,
+      "right": 100,
+      "bottom": 40,
+      "left": 120
+    },
+    "md": {
+      "top": 10,
+      "right": 120,
+      "bottom": 40,
+      "left": 150
+    },
+    "lg": {
+      "top": 10,
+      "right": 120,
+      "bottom": 40,
+      "left": 200
+    }
+  },
+  "mobileBreakpoint": 414,
+  "mediumBreakpoint": 600,
+
+  "x_num_ticks": {
+    "sm": 3,
+    "md": 5,
+    "lg": 5
+  }
+};
